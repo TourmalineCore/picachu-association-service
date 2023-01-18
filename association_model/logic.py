54b2c43014b897224588b1fb18e7d5191c07e632
Maria Yadryshnikova <93837471+myadryshnikova@users.noreply.github.com>
2023-01-18 17:08:42 +0500

    feat(1st version): #861m8antt: add the first version of the service

---
+++ association_model/logic.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision
+from transformers import pipeline
+from datetime import datetime
+
+print(torch.__version__)
+print(torchvision.__version__)
+
+with open('./association_model/association_dict.txt', 'r') as dict_file:
+    CANDIDATE_LABELS = dict_file.read().split("\n")
+
+# CANDIDATE_LABELS = ['calm', 'people', 'plant']
+
+MODEL_NAME = 'cross-encoder/nli-distilroberta-base'
+
+MODEL = pipeline("zero-shot-classification", model=MODEL_NAME)
+
+
+class ModelLogic:
+    def __init__(self):
+        pass
+
+    def model_specific_logic(self, sequence_to_classify):
+        started_time = datetime.now()
+        print(sequence_to_classify)
+        print(CANDIDATE_LABELS)
+        predict = MODEL(sequence_to_classify, CANDIDATE_LABELS, multi_label=True)
+        print(predict)
+
+        result = [predict['labels'][i] for i in range(len(predict['labels'])) if predict['scores'][i] > 0.7]
+        print(result)
+
+        ended_time = datetime.now()
+        print(f'TIME:{ended_time - started_time}')
+
+        return [{'name': tag} for tag in result]
